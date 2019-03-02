d54d90771f39dfcc576056a66b229310d3c0a4c0
Grigory <ivanovgrigoriinikolaevich00@gmail.com>
2019-03-02 12:29:33 +0700

    finished lab1

--- lab1/lab1.srcs/sim_1/new/SourceTestbench.sv
+++ lab1/lab1.srcs/sim_1/new/SourceTestbench.sv
@@ -23,12 +23,17 @@
 module SourceTestbench(
     );
     logic ld, rd, lamp;
-    Source src(ld,rd,lamp);
+    logic toggle, light1, light2, lights[7:0];
+    Source src(ld,rd,lamp,
+    toggle, light1, light2, lights);
     
     initial begin
     $display("Running testbench");
     ld = 0;
     rd = 0; // Обе двери сначала закрыты
+    toggle = 1;
+    light1 = 1;
+    light2 = 1;
     #5 // Задержка в 5 единиц времени
     ld = 1; // Открыта левая дверь
     #2
@@ -38,6 +43,18 @@ module SourceTestbench(
     #1
     rd = 0; // Правую дверь закрыли
     #2
+    toggle = 0;
+    #5
+    toggle = 1;
+    #5
+    light1 = 0;
+    #5
+    light2 = 0;
+    #5
+    light1 = 1;
+    #5
+    light2 = 1;
+    #5
     $finish;
     end
     
