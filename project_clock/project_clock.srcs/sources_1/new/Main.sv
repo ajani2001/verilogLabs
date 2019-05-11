2bfe6b7b995f35ded9b98dd1419fb951246f1a7e
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2019-05-11 12:31:01 +0700

    final clock version

--- project_clock/project_clock.srcs/sources_1/new/Main.sv
+++ project_clock/project_clock.srcs/sources_1/new/Main.sv
@@ -5,7 +5,6 @@ module Main(input clock, input resetBtn, output [7:0] output7seg0, output [7:0] 
     reg [3:0] seg1 = 4'b0;
     reg [3:0] seg2 = 4'b0;
     reg [3:0] seg3 = 4'b0;
-    reg [13:0] timeStorage = 14'b0;
     reg [24:0] tickStorage = 25'b0;
     always @(posedge clock) begin
         if(~resetBtn) begin
@@ -13,15 +12,14 @@ module Main(input clock, input resetBtn, output [7:0] output7seg0, output [7:0] 
             seg1 <= 4'b0;
             seg2 <= 4'b0;
             seg3 <= 4'b0;
-            timeStorage <= 14'b0;
             tickStorage <= 25'b0;
         end else begin
             if(tickStorage == 32767999) begin
                 tickStorage <= 0;
                 seg0 = seg0 == 9? 0 : seg0 + 1;
-                seg1 = seg0 == 0? seg1 == 9? 0 : seg1 + 1 : seg1;
+                seg1 = seg0 == 0? seg1 == 5? 0 : seg1 + 1 : seg1;
                 seg2 = seg0 == 0 && seg1 == 0? seg2 == 9? 0 : seg2 + 1 : seg2;
-                seg3 = seg0 == 0 && seg1 == 0 && seg2 == 0? seg3 == 9? 0 : seg3 + 1 : seg3;
+                seg3 = seg0 == 0 && seg1 == 0 && seg2 == 0? seg3 == 5? 0 : seg3 + 1 : seg3;
             end else begin
                 tickStorage <= tickStorage + 1;
             end
